3c82c70a5759b558f8f84deac8b07d75366dd8d7
OmkarRobatics <omkarseth008@gmail.com>
2023-02-26 15:04:58 -0500

    Initial commit

---
+++ src/Task_7.java
@@ -0,0 +1,15 @@
+public class Task_5 {
+    public static void main(String[] args) {
+        final int ROW = 5;
+        final int COL = 5;
+
+        for (int row = 0; row < ROW; row++)
+        {
+            for(int col = 0; col<= COL; col++)
+            {
+                System.out.printf("*");
+            }
+            System.out.println("");
+        }
+    }
+}
